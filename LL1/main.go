ddc73afa51429a32cdafb368c307ec5ebd3c3e95
unknown <kekemuyu@126.com>
2019-12-20 17:54:08 +0800

    LL1 add some funcs

--- LL1/main.go
+++ LL1/main.go
@@ -8,30 +8,52 @@ import (
 	"strings"
 )
 
+type sets struct {
+	Fists  []string
+	Fllows []string
+}
+
+var LLTable map[string]*sets
+
+func init() {
+	LLTable = make(map[string]*sets)
+}
+
 func main() {
 	fmt.Println(getVar())
 }
 
-//得到非终结符,非终结符像变量用var命名
-func getVar() []string {
+//从产生式获取firsts
+func getFirsts(expres []string, vars []string) []string {
+
+}
+
+//从产生式获取fellows
+func getFellows(expres []string, vars []string) []string {
+
+}
+
+//得到非终结符和产生式,非终结符像变量用var命名
+func getVar() ([]string, []string) {
 	f, err := os.Open("test.l")
 	if err != nil {
 		panic(err)
 	}
 	br := bufio.NewReader(f)
 	vars := make([]string, 0)
+	expresion := make([]string, 0)
 	for {
 		t, _, err := br.ReadLine()
 		if err == io.EOF {
-			return vars
+			return expresion, vars
 		}
 		if len(t) <= 0 {
 			continue
 		}
-		ts := strings.Trim(string(t))
-
+		ts := strings.Replace(string(t), " ", "", -1)
+		expresion = append(expresion, ts)
 		index := strings.Index(ts, "->")
 		vars = append(vars, ts[:index])
 	}
-	return vars
+	return expresion, vars
 }
